c398310c2863c46a4975f7bc5e0f350d214c495a
zolangi <zolangi@users.noreply.github.com>
2016-11-09 12:26:25 -0800

    format triangle.js

--- javascript/triangle.js
+++ javascript/triangle.js
@@ -1,6 +1,6 @@
 for(var i=0; i < 8; i++){
-			for(var j = 0; j < i; j++){
-			console.log("#");
-			}
-			console.log("\n");
-		}
+	for(var j = 0; j < i; j++){
+		console.log("#");
+	}
+	console.log("\n");
+}
