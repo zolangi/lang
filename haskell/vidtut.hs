e78d098bfdb36001832c7a4eba65243e4ccd9231
zolangi <zolangi@users.noreply.github.com>
2016-11-17 16:50:50 -0800

    Update vidtut.hs

--- haskell/vidtut.hs
+++ haskell/vidtut.hs
@@ -13,13 +13,13 @@ always5 = 5
 
 sumOfNums = sum[1..1000]
 
-addEx = 5+4
-subEx = 5-4
-multEx = 5*4
-divEx = 5/4
+addNums x y = x + y
+subNums x y = x - y
+multNums x y = x * y
+divNums x y = x/y
 
-modEx = mod 5 4
-modEx2 = 5 `mod` 4
+modNums x y = mod x y
+modAlt x y = x `mod` y
 
 negNumEx = 5 + (-4) --Has to be in parantheses or else Haskell will get confused
 
