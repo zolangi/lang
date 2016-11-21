97fb5ab97499da95f7b4c7bbb5617b723ce4f20f
zolangi <zolangiramirez@gmail.com>
2016-11-21 09:47:59 -0800

    Update bmi.hs

--- haskell/bmi.hs
+++ haskell/bmi.hs
@@ -1,5 +1,6 @@
 bmiType :: (RealFloat a) => a -> a -> String
-bmiType weight height
+bmiType weight height 
+{-Weight is taken in Kilograms and height in Meters-}
 	| bmi <= underweight = "You are underweight"
 	| bmi <= normal = "You are at a normal weight"
 	| bmi <= overweight = "You are overweight"
