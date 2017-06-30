80e87607cdf7cca3800e42d5d88991434e74dffe
zolangi <zolangiramirez@gmail.com>
2017-06-29 17:17:45 -0700

    Create min.js

---
+++ javascript/min.js
@@ -0,0 +1,9 @@
+console.log("Hi! this program returns the minimum between two numbers\n");
+
+var x = 125
+var y = 5;
+var min = function (x, y){
+  return Math.min(x,y);
+}
+
+console.log("The minimum number is: " + min(x,y));
