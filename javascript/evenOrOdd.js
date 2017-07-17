124f1761d309b36f325935f0a207a64c614255a1
Zolangi Ramirez <zolangi@users.noreply.github.com>
2017-07-17 15:58:33 -0700

    Update evenOrOdd.js

--- javascript/evenOrOdd.js
+++ javascript/evenOrOdd.js
@@ -1,12 +1,12 @@
 var even = false;
 
 var isEven = function(x){
-	if(x = 0)
+	if(x == 0)
 		return even = true;
-	else if (x = 1)
+	else if (x == 1)
 		return even;
-	else{
-		x = x-2;
-		isEven(x);
-	}
+	else if (x < 0)
+		isEven(-x);
+	else
+		isEven(x-2);
 }
